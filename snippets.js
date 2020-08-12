3d41cf26a3b5c69989c5fc811c9b93823ac39f32
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-08-12 13:20:51 +0200

    Create snippets.js
    createAnyElement

---
+++ snippets.js
@@ -0,0 +1,7 @@
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k])
+    }
+    return element;
+}
