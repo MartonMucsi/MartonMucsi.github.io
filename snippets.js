d1253d74fe2fb2c9831c539259ef8546231e5119
martonmucsi <47630516+martonmucsi@users.noreply.github.com>
2020-08-12 17:28:31 +0200

    scope, bt

--- snippets.js
+++ snippets.js
@@ -5,3 +5,25 @@ function createAnyElement(name, attributes) {
     }
     return element;
 }
+
+/*Scope demonstration: var is function scoped, let is block scope*/
+var globalName = "Joe";
+function something(){
+    var globalName = "Jack"
+    console.log(globalName)
+}
+something
+console.log(globalName)
+
+let name = "Joe";
+function something2(){
+    let name = "Jack"
+    console.log(name)
+}
+something2()
+console.log(name)
+
+/* Arrow functions*/
+let adder = (n1, n2) => n1 + n2;
+
+let greeter = name => `Hello ${name}!`;
